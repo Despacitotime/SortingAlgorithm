53724deb5fd168349928398fc43e74a7e4bfe9d4
Despacitotime <941058084@qq.com>
2021-05-10 13:27:33 +0800

    Sort is the basic of all sort algorithms,and it contains the basis method

---
+++ src/Sort.java
@@ -0,0 +1,11 @@
+/**
+ * @author xxx
+ * @date 2021/5/10 13:25
+ */
+public class Sort {
+    public void swapTwoNumber(int preNumber, int lastNumber){
+        int temp = preNumber;
+        preNumber = lastNumber;
+        lastNumber = temp;
+    }
+}
